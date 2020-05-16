11c19581c2da054f0155e54bf3848c27ce510216
Elad Motzny <emotzny@gmail.com>
2020-05-16 21:16:36 +0300

    Need to fix wall collision

---
+++ Brick Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This class will do everything concerning the paddle
+public class Paddle : MonoBehaviour
+{
+    [Tooltip("Change to increase/decrease movement speed")] [SerializeField] float moveSpeed = 1f;//10 seems to be fine in terms of speed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves the paddle according to user input
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
---
+++ Brick Breaker/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    /*
+     * THIS CLASS WILL BE USED TO KEEP TRACK ON PLAYER STATS LIKE LIVES AND SCORE
+     * 
+     * 
+     * 
+     */
+
+    public static int playerScore = 0;
+    public static int playerLives = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Inceases the player score
+    public void increaseScore(int score)
+    {
+        playerScore += score;
+    }
+
+    //decreases the player lives by 1
+    public void decreaseLives()
+    {
+        playerLives--;
+        if(playerLives == 0)
+        {
+            //call game over function/scene(?)
+        }
+    }
+}
