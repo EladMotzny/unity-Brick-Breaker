22bb6ccc9ca47f024dde6bd47243debcced28a36
Anna <33619352+annty2@users.noreply.github.com>
2020-05-17 16:49:54 +0300

    place the ball on the paddle when goes out of frame

--- Brick Breaker/Assets/Scripts/Ball.cs
+++ Brick Breaker/Assets/Scripts/Ball.cs
@@ -6,19 +6,31 @@ public class Ball : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        rb.AddForce(Vector2.up * 500);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!inPlay)
+        {
+            transform.position = paddle.position; // move the ball to the middle of the paddle
+        }
+
+        if (Input.GetButtonDown("Jump") && !inPlay) // space bar
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,6 +38,8 @@ public class Ball : MonoBehaviour
         if (collision.CompareTag("bottom"))
         {
             Debug.Log("Bootom of screen");
+            rb.velocity = Vector2.zero; // zero out the forces on the ball
+            inPlay = false;
         }
     }
 }
